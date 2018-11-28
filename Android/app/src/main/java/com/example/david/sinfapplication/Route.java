a1cfaff7881bb07dd5c8eddb07b60829021d3628
Davide Costa <up201503995@fe.up.pt>
2018-11-28 22:54:20 +0000

    Added Route class and JSON jar

---
+++ Android/app/src/main/java/com/example/david/sinfapplication/Route.java
@@ -0,0 +1,7 @@
+package com.example.david.sinfapplication;
+
+public class Route
+{
+    private static String PrimaveraWebAPIDefaultRoute = "dservers.ddns.net:2018/WebApi/";
+    public static String Authentication = PrimaveraWebAPIDefaultRoute + "token";
+}
