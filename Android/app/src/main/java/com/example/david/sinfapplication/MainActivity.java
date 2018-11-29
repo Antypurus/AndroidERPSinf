da66ed952a1a340c519c844201217c92f7d7fab1
Tiago José Sousa Magalhães <crakylps@gmail.com>
2018-11-29 17:31:31 +0000

    created register order view in landscape

--- Android/app/src/main/java/com/example/david/sinfapplication/MainActivity.java
+++ Android/app/src/main/java/com/example/david/sinfapplication/MainActivity.java
@@ -67,7 +67,6 @@ public class MainActivity extends AppCompatActivity
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
-
         // Checks the orientation of the screen
         if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
             Toast.makeText(this, "landscape", Toast.LENGTH_SHORT).show();
