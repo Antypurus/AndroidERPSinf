cd80392d50e9deb2637a44c1039cf5bd367f3946
Dinis Trigo <up201504196@fe.up.pt>
2018-11-30 01:58:36 +0000

    Added class to parse view customer response from server

---
+++ Android/app/src/main/java/com/example/david/sinfapplication/WebAPI/Parsers/CustomerParser.java
@@ -0,0 +1,12 @@
+package com.example.david.sinfapplication.WebAPI.Parsers;
+
+import com.example.david.sinfapplication.CommonDataClasses.Customer;
+
+public class CustomerParser
+{
+
+    public static Customer parseViewCustomerRequestResponse(String viewCustomerRequestResponse)
+    {
+        
+    }
+}
