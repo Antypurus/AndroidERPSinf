b152f0a8cb844f721b764ebd103d9235c01fb4a7
Davide Costa <up201503995@fe.up.pt>
2018-11-30 09:55:07 +0000

    stuff

--- Android/app/src/main/java/com/example/david/sinfapplication/WebAPI/Communication/Route.java
+++ Android/app/src/main/java/com/example/david/sinfapplication/WebAPI/Communication/Route.java
@@ -2,8 +2,8 @@ package com.example.david.sinfapplication.WebAPI.Communication;
 
 public class Route
 {
-    public static final String serverHostName = "dservers.ddns.net";
-    public static final String serverPort = "2018";
+    private static final String serverHostName = "dservers.ddns.net";
+    private static final String serverPort = "2018";
     private static String PrimaveraWebAPIDefaultRoute = "http://" + serverHostName + ":" + serverPort + "/WebApi/";
     private static String PrimaveraWebAPIDirectQuery = "Administrador/Consulta";
     public static String Authentication = PrimaveraWebAPIDefaultRoute + "token";
