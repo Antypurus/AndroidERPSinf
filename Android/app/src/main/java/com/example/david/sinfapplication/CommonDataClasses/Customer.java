72d5b8076945ed8aa0bd181d50df0950d8341711
Dinis Trigo <up201504196@fe.up.pt>
2018-11-30 00:59:39 +0000

    Added class Customer and respective fields

---
+++ Android/app/src/main/java/com/example/david/sinfapplication/CommonDataClasses/Customer.java
@@ -0,0 +1,71 @@
+package com.example.david.sinfapplication.CommonDataClasses;
+
+public class Customer
+{
+    /**
+     * Corresponds to Cliente in primavera
+     */
+    private String id;
+    /**
+     * Corresponds to Nome in primavera
+     */
+    private String name;
+    /**
+     * Corresponds to Descricao in primavera
+     */
+    private String description;
+    /**
+     * Corresponds to Morada in primavera
+     */
+    private String Address;
+    /**
+     * Corresponds to Localidade in primavera
+     */
+    private String city;
+    /**
+     * Corresponds to CodigoPostal in primavera
+     */
+    private String postalCode;
+    /**
+     * Corresponds to LocalidadeCodigoPostal in primavera
+     */
+    private String postalCodeCity;
+    /**
+     * Corresponds to Telefone in primavera
+     */
+    private String phoneNumber;
+    /**
+     * Corresponds to Fax in primavera
+     */
+    private String faxNumber;
+    /**
+     * Corresponds to EnderecoWeb in primavera
+     */
+    private String webSite;
+    /**
+     * Corresponds to Distrito in primavera
+     */
+    private String State;
+    /**
+     * Corresponds to NumContribuinte in primavera
+     */
+    private String taxNumber;
+    /**
+     * Corresponds to Pais in primavera
+     */
+    private String country;
+    /**
+     * Corresponds to Moeda in primavera
+     */
+    private String currency;
+    /**
+     * Corresponds to DebitoContaCorrente in primavera
+     */
+    private String checkingAccountDebit;
+    /**
+     * Corresponds to DebitoEncomendasPendentes in primavera
+     */
+    private String pendingOrdersDebit;
+
+
+}
