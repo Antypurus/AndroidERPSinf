c06deda74169c292d8eaee04858b062f36886570
Davide Costa <up201503995@fe.up.pt>
2018-11-30 09:55:23 +0000

    stuff

--- Android/app/src/main/java/com/example/david/sinfapplication/WebAPI/Communication/Route.java
+++ Android/app/src/main/java/com/example/david/sinfapplication/WebAPI/Communication/Route.java
@@ -8,6 +8,6 @@ public class Route
     private static String PrimaveraWebAPIDirectQuery = "Administrador/Consulta";
     public static String Authentication = PrimaveraWebAPIDefaultRoute + "token";
     public static String ListProducts = PrimaveraWebAPIDefaultRoute + PrimaveraWebAPIDirectQuery;
-    public static String viewCustomer = PrimaveraWebAPIDefaultRoute + "Base/Clientes/Edita/C0001";
+    public static String viewCustomer = PrimaveraWebAPIDefaultRoute + "Base/Clientes/Edita";
 
 }
