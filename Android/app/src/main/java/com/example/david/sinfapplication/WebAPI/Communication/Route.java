46ed0511d8fc46b528837a53352985ab1aa18683
Dinis Trigo <up201504196@fe.up.pt>
2018-11-30 01:45:01 +0000

    added route for view customer

--- Android/app/src/main/java/com/example/david/sinfapplication/WebAPI/Communication/Route.java
+++ Android/app/src/main/java/com/example/david/sinfapplication/WebAPI/Communication/Route.java
@@ -6,5 +6,6 @@ public class Route
     private static String PrimaveraWebAPIDirectQuery = "Administrador/Consulta";
     public static String Authentication = PrimaveraWebAPIDefaultRoute + "token";
     public static String ListProducts = PrimaveraWebAPIDefaultRoute + PrimaveraWebAPIDirectQuery;
+    public static String viewCustomer = PrimaveraWebAPIDefaultRoute + PrimaveraWebAPIDirectQuery;
 
 }
