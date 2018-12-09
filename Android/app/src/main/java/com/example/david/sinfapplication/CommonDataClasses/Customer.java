101d5e113f3e4ccf407731f6398b626dc49a932d
Dinis Trigo <up201504196@fe.up.pt>
2018-12-09 03:53:17 +0000

    added class Customer to be superclass for customer detailed and customer of salesman

---
+++ Android/app/src/main/java/com/example/david/sinfapplication/CommonDataClasses/Customer.java
@@ -0,0 +1,19 @@
+package com.example.david.sinfapplication.CommonDataClasses;
+
+public class Customer
+{
+    /**
+     * The id of the customer
+     */
+    protected String id;
+    /**
+     * The name of the customer
+     */
+    protected String name;
+
+    public Customer(String id, String name)
+    {
+        this.id = id;
+        this.name = name;
+    }
+}
