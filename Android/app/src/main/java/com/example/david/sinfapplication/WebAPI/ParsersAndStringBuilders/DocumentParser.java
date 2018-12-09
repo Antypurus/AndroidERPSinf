a74a4a8e35e5946656992e18dd1d4f1425088090
Davide Costa <up201503995@fe.up.pt>
2018-12-09 00:40:06 +0000

    added parseViewCustomerDocumentRequestResponse

--- Android/app/src/main/java/com/example/david/sinfapplication/WebAPI/ParsersAndStringBuilders/DocumentParser.java
+++ Android/app/src/main/java/com/example/david/sinfapplication/WebAPI/ParsersAndStringBuilders/DocumentParser.java
@@ -1,16 +1,67 @@
 package com.example.david.sinfapplication.WebAPI.ParsersAndStringBuilders;
 
 import com.example.david.sinfapplication.CommonDataClasses.Document;
-import com.example.david.sinfapplication.CommonDataClasses.Product;
+import com.example.david.sinfapplication.CommonDataClasses.DocumentLine;
 
+import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class DocumentParser
 {
-    public static Document parseViewProductRequestResponse(String viewProductRequestResponse) throws
+    public static Document parseViewCustomerDocumentRequestResponse(String viewCustomerDocumentRequestResponse) throws
             JSONException
     {
-        //TODO
-        return new Document();
+        JSONObject dataSetObject = new JSONObject(viewCustomerDocumentRequestResponse).getJSONObject("DataSet");
+        JSONArray arrayWithDocument = dataSetObject.getJSONArray("Table");
+
+        assert arrayWithDocument.length() == 1;
+
+        JSONObject line = arrayWithDocument.getJSONObject(0);
+        String id = line.getString("Id");
+        String docType = line.getString("TipoDoc");
+        String serie = line.getString("Serie");
+        String docNumber = line.getString("NumDoc");
+        String documentTotal = line.getString("TotalDocumento");
+        String date = line.getString("Data");
+        String state = line.getString("Estado");
+
+        return new Document(id, docType, serie, docNumber, documentTotal, date, state);
+    }
+
+    public static ArrayList<DocumentLine> parseViewDocumentDetailsRequestResponse(String viewDocumentDetailsRequestResponse) throws
+            JSONException
+    {
+        ArrayList<DocumentLine> documentLines = new ArrayList<>();
+        JSONObject dataSetObject = new JSONObject(viewDocumentDetailsRequestResponse).getJSONObject("DataSet");
+        JSONArray documentLinesArray = dataSetObject.getJSONArray("Table");
+
+        int numberDocumentLines = documentLinesArray.length();
+        for(int i = 0; i < numberDocumentLines; i++)
+        {
+            JSONObject line = documentLinesArray.getJSONObject(i);
+            int number = line.getInt("NumLinha");
+            String productId = line.getString("Artigo");
+            String productDescription = line.getString("Descricao");
+            int discount = line.getInt("Desconto1");
+            int commercialDiscount = line.getInt("DescontoComercial");
+            int taxes = line.getInt("TaxaIva");
+            int quantity = line.getInt("Quantidade");
+            int unitaryPrice = line.getInt("PrecUnit");
+            int netPrice = line.getInt("NumLinha");
+            String date = line.getString("PrecUnit");
+            String outDate = line.getString("DataSaida");
+            String deliveryDate = line.getString("DataEntrega");
+            int comission = line.getInt("Comissao");
+            String idCabecDoc = line.getString("IdCabecDoc");
+
+            DocumentLine documentLine = new DocumentLine(number, productId, productDescription, discount, commercialDiscount, taxes,
+                    quantity, unitaryPrice, netPrice, date, outDate, deliveryDate, comission, idCabecDoc);
+            documentLines.add(documentLine);
+        }
+
+        return documentLines;
     }
 }
