5d39c8fafe173f0629db0d0d485a3bbae764db99
Dinis Trigo <up201504196@fe.up.pt>
2018-12-09 01:34:56 +0000

    added parse product code on method parseViewProductRequestResponse

--- Android/app/src/main/java/com/example/david/sinfapplication/WebAPI/ParsersAndStringBuilders/ProductParser.java
+++ Android/app/src/main/java/com/example/david/sinfapplication/WebAPI/ParsersAndStringBuilders/ProductParser.java
@@ -3,13 +3,25 @@ package com.example.david.sinfapplication.WebAPI.ParsersAndStringBuilders;
 import com.example.david.sinfapplication.CommonDataClasses.Product;
 
 import org.json.JSONException;
+import org.json.JSONObject;
 
 public class ProductParser
 {
     public static Product parseViewProductRequestResponse(String viewProductRequestResponse) throws
             JSONException
     {
-        //TODO
-        return new Product(null, null, null,null, null, 0, 0, null);
+        JSONObject dataSetObject = new JSONObject(viewProductRequestResponse);
+        String id = dataSetObject.getString("Artigo");
+        String family = dataSetObject.getString("Familia");
+        String subfamily = dataSetObject.getString("SubFamilia");
+        String description = dataSetObject.getString("Descricao");
+        String observations = dataSetObject.getString("Observacoes");
+        //TODO falta parsar a imagem
+        int currentStock = dataSetObject.getInt("StkActual");
+        double pvp = dataSetObject.getDouble("PVP1");
+        String currency = dataSetObject.getString("Moeda");
+
+        return new Product(id, family, subfamily, description,
+                observations, currentStock, pvp, currency);
     }
 }
