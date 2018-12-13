09e8949defb78587b1a3d70d1a0a2303d68da4f8
Davide Costa <up201503995@fe.up.pt>
2018-12-13 10:35:07 +0000

    added BasicDocument and DetailedDocument

---
+++ Android/app/src/main/java/com/example/david/sinfapplication/CommonDataClasses/DetailedDocument.java
@@ -0,0 +1,14 @@
+package com.example.david.sinfapplication.CommonDataClasses;
+
+
+public class DetailedDocument extends Document
+{
+
+
+    public DetailedDocument(String id, String docType, String series, String docNumber, String documentTotal, String date, String state)
+    {
+        super(id, docType, series, docNumber, documentTotal, date, state);
+    }
+
+
+}
