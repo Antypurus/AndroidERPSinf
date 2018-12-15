c8abdc2f35ac34950bd75ae4a2ecc4c224d7f765
Tiago José Sousa Magalhães <crakylps@gmail.com>
2018-12-15 19:31:02 +0000

    created login view and activity, need to now work on login code

--- Android/app/src/main/java/com/example/david/sinfapplication/Activities/Login/login_activity.java
+++ Android/app/src/main/java/com/example/david/sinfapplication/Activities/Login/login_activity.java
@@ -2,10 +2,11 @@ package com.example.david.sinfapplication.Activities.Login;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.example.david.sinfapplication.R;
 
-public class login_activity extends Activity {
+public class login_activity extends AppCompatActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState)
