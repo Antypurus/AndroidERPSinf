6cf3f2f32597737ba5676555c4cda9deae569cf2
Tiago José Sousa Magalhães <crakylps@gmail.com>
2018-12-15 19:09:15 +0000

    created login view and activity, need to now work on login code

---
+++ Android/app/src/main/java/com/example/david/sinfapplication/Activities/Login/login_activity.java
@@ -0,0 +1,17 @@
+package com.example.david.sinfapplication.Activities.Login;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.david.sinfapplication.R;
+
+public class login_activity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_layout);
+    }
+
+}
--- Android/app/src/main/java/com/example/david/sinfapplication/Activities/edit_customer/edit_customer_activity.java
+++ Android/app/src/main/java/com/example/david/sinfapplication/Activities/edit_customer/edit_customer_activity.java
@@ -1,8 +1,14 @@
 package com.example.david.sinfapplication.Activities.edit_customer;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 
+import com.example.david.sinfapplication.Activities.register_order.register_order_activity;
 import com.example.david.sinfapplication.R;
 
 public class edit_customer_activity extends Activity {
@@ -12,6 +18,15 @@ public class edit_customer_activity extends Activity {
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.edit_customer);
+        ProgressBar bar = (ProgressBar) findViewById(R.id.creation_progress);
+        bar.getProgressDrawable().mutate().setColorFilter(Color.RED,PorterDuff.Mode.SRC_IN);
+    }
+
+    public void sendMessage(View view)
+    {
+        // prototype, change to the checkout view
+        Intent intent = new Intent(this, register_order_activity.class);
+        startActivity(intent);
     }
 
 }
