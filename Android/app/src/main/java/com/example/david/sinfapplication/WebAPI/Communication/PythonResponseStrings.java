f01057e848fbea06d37972567fdf090f98386b6b
Dinis Trigo <up201504196@fe.up.pt>
2018-12-16 01:26:26 +0000

    added class for defining python response strings

---
+++ Android/app/src/main/java/com/example/david/sinfapplication/WebAPI/Communication/PythonResponseStrings.java
@@ -0,0 +1,6 @@
+package com.example.david.sinfapplication.WebAPI.Communication;
+
+public class PythonResponseStrings
+{
+    public static final String invalidUsernameOrPassword = "Error: Invalid Password Username Combo";
+}
