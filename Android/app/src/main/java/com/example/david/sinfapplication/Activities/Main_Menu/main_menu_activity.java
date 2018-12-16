81af38c1111aca9ce168222ddb1bf77ffff9e2ea
Tiago José Sousa Magalhães <crakylps@gmail.com>
2018-12-16 18:50:24 +0000

    refactored main menu, modified the login procces to take us to the main menu instead of the product catalog, also created a an activity to handle the main menu but it is still missing the button linkking

---
+++ Android/app/src/main/java/com/example/david/sinfapplication/Activities/Main_Menu/main_menu_activity.java
@@ -0,0 +1,17 @@
+package com.example.david.sinfapplication.Activities.Main_Menu;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.david.sinfapplication.R;
+
+public class main_menu_activity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu);
+    }
+
+}
