41b170801c9fc43bf4c49d1f0150e3dc76e9d8ec
Dinis Trigo <up201504196@fe.up.pt>
2018-12-16 19:37:24 +0000

    added constructor that computes the id internally for class CustoemrBasic

--- Android/app/src/main/java/com/example/david/sinfapplication/CommonDataClasses/CustomerBasic.java
+++ Android/app/src/main/java/com/example/david/sinfapplication/CommonDataClasses/CustomerBasic.java
@@ -1,5 +1,10 @@
 package com.example.david.sinfapplication.CommonDataClasses;
 
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class CustomerBasic extends Customer
 {
 
@@ -19,6 +24,31 @@ public class CustomerBasic extends Customer
     public CustomerBasic(String id, String name, String address, String phoneNumber, String taxNumber)
     {
         super(id, name);
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.taxNumber = taxNumber;
+    }
+
+    public CustomerBasic(String name, String address, String phoneNumber, String taxNumber)
+    {
+        super("", name);
+        //TODO ver se isto ta a funcionar direito;
+        String fullObjString = name + address + phoneNumber + taxNumber;
+        MessageDigest digest = null;
+        try
+        {
+            digest = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e)
+        {
+            e.printStackTrace();
+            Log.e("CustomerBasic", "Error computing SHA-256 of CustomerBasic");
+        }
+        byte hash[] = digest.digest(fullObjString.getBytes());
+        StringBuilder hashSB = new StringBuilder();
+        for (byte b : hash)
+            hashSB.append(String.format("%02X ", b));
+
+        this.id = hashSB.toString();
         this.address = address;
         this.phoneNumber = phoneNumber;
         this.taxNumber = taxNumber;
