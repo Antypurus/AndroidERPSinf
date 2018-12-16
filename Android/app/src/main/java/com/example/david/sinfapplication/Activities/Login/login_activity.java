106504032abc0f3d7eab4e48a6bdfb0d0e4dd4d9
Tiago José Sousa Magalhães <crakylps@gmail.com>
2018-12-16 17:09:46 +0000

    added login to android

--- Android/app/src/main/java/com/example/david/sinfapplication/Activities/Login/login_activity.java
+++ Android/app/src/main/java/com/example/david/sinfapplication/Activities/Login/login_activity.java
@@ -1,11 +1,16 @@
 package com.example.david.sinfapplication.Activities.Login;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 import com.example.david.sinfapplication.R;
+import com.example.david.sinfapplication.WebAPI.WebAPI;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
 
 public class login_activity extends AppCompatActivity {
 
@@ -18,7 +23,44 @@ public class login_activity extends AppCompatActivity {
 
     public void execute_login(View view)
     {
+        String username = ((EditText)this.findViewById(R.id.username)).getText().toString();
+        String password = ((EditText)this.findViewById(R.id.password)).getText().toString();
 
+        if(username.equals("")||password.equals(""))
+        {
+            Log.d("Primavera Login","No Username or password suplied");
+            return;
+        }
+
+        WebAPI.loginResult result = null;
+        try {
+            WebAPI.login(username,password);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        }
+
+        if(result!=null)
+        {
+            if(result.equals(WebAPI.loginResult.loginSucessfull))
+            {
+                //go to main menu
+                Log.d("Primavera Login","Login Successfull");
+            }
+            else if(result.equals(WebAPI.loginResult.loginFailedWrongUsernameOrPassword))
+            {
+                // wrong username password combo error msg
+                Log.d("Primavera Login","Wrong Username/password comobo");
+            }
+            else
+            {
+                // server error msg
+                Log.d("Primavera Login","Login Server Error");
+            }
+        }
     }
 
 }
