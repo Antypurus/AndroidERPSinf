b77ab3a13b1d7d406afe438754cd1b0546fbb269
Dinis Trigo <up201504196@fe.up.pt>
2018-12-16 18:46:26 +0000

    created new class CustomerBasic that has less details

---
+++ Android/app/src/main/java/com/example/david/sinfapplication/CommonDataClasses/CustomerBasic.java
@@ -0,0 +1,10 @@
+package com.example.david.sinfapplication.CommonDataClasses;
+
+public class CustomerBasic extends Customer
+{
+
+    public CustomerBasic(String id, String name)
+    {
+        super(id, name);
+    }
+}
