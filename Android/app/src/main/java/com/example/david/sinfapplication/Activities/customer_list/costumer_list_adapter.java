dff544ef28ea4b921c3028b5224c36dfc27d49a9
Tiago José Sousa Magalhães <crakylps@gmail.com>
2018-12-18 11:10:49 +0000

    working on costumer call

--- Android/app/src/main/java/com/example/david/sinfapplication/Activities/customer_list/costumer_list_adapter.java
+++ Android/app/src/main/java/com/example/david/sinfapplication/Activities/customer_list/costumer_list_adapter.java
@@ -1,11 +1,16 @@
 package com.example.david.sinfapplication.Activities.customer_list;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.david.sinfapplication.Activities.view_customer.view_customer_activity;
 import com.example.david.sinfapplication.CommonDataClasses.CustomerOfSalesman;
 import com.example.david.sinfapplication.R;
 
@@ -26,6 +31,7 @@ public class costumer_list_adapter extends RecyclerView.Adapter<costumer_list_ad
         public ConstraintLayout costumer = null;
         public TextView costumer_name = null;
         public ConstraintLayout costumer_layout = null;
+        public Button costumer_call_button = null;
 
         public costumer_list_holder(ConstraintLayout costumer) {
             super(costumer);
@@ -33,6 +39,7 @@ public class costumer_list_adapter extends RecyclerView.Adapter<costumer_list_ad
             this.costumer = costumer;
             this.costumer_name = (TextView)this.costumer.findViewById(R.id.costumer_name);
             this.costumer_layout = (ConstraintLayout)this.costumer.findViewById(R.id.costumer_layout);
+            this.costumer_call_button = (Button)this.costumer.findViewById(R.id.costumer_call_button);
         }
     }
 
@@ -49,11 +56,26 @@ public class costumer_list_adapter extends RecyclerView.Adapter<costumer_list_ad
     @Override
     public void onBindViewHolder(costumer_list_holder holder, int position) {
         holder.costumer_name.setText(this.customers.get(position).getName());
+
+        holder.costumer_layout.setOnClickListener(view -> goToCustomer(holder.costumer,customers.get(position).getId()));
+        holder.costumer_call_button.setOnClickListener(view -> callClient(holder.costumer_call_button,customers.get(position)));
     }
 
     @Override
     public int getItemCount() {
         return this.customers.size();
+    }
+
+    public void goToCustomer(View view,String client)
+    {
+        Intent intent = new Intent(view.getContext(),view_customer_activity.class);
+        intent.putExtra("customerId",client);
+        view.getContext().startActivity(intent);
+    }
+
+    public void callClient(View view,String client_number)
+    {
+        view.getContext().startActivity(new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", client_number, null)));
     }
 
 }
