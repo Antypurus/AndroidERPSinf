60fcc3ff4002e432188b8e07470549483d633996
Tiago José Sousa Magalhães <crakylps@gmail.com>
2018-12-19 11:50:54 +0000

    modded query to get customer and declared sales order view in the manifest

--- Android/app/src/main/java/com/example/david/sinfapplication/WebAPI/WebAPI.java
+++ Android/app/src/main/java/com/example/david/sinfapplication/WebAPI/WebAPI.java
@@ -89,7 +89,7 @@ public class WebAPI
         String query = "\"SELECT Cliente, Nome FROM Clientes WHERE Vendedor = \'" + salesmanId + "\'\"";
 
         //TODO
-        query = "\"SELECT Cliente, Nome, Fac_Tel FROM Clientes WHERE Cliente = '1'\"";
+        query = "\"SELECT Cliente, Nome, Fac_Tel FROM Clientes WHERE Vendedor = '"+salesmanId+"'\"";
         String viewCustomersOfSalesmanResponse = PrimaveraWebAPI.sendRequest(requestRoute, RequestMethod.listCustomersOfASalesman,
                 ContentType.ApplicationJson, query.getBytes());
         try
