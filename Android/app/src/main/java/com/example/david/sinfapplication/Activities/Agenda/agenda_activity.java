9f169db8f0533ba8fbf321b539413326ba6fc567
Tiago José Sousa Magalhães <crakylps@gmail.com>
2018-12-20 01:04:32 +0000

    agenda set up to load

--- Android/app/src/main/java/com/example/david/sinfapplication/Activities/Agenda/agenda_activity.java
+++ Android/app/src/main/java/com/example/david/sinfapplication/Activities/Agenda/agenda_activity.java
@@ -6,10 +6,13 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import com.example.david.sinfapplication.CommonDataClasses.AgendaEntry;
+import com.example.david.sinfapplication.CommonDataClasses.CommonStorage;
 import com.example.david.sinfapplication.R;
 import com.example.david.sinfapplication.WebAPI.WebAPI;
 
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
 
 public class agenda_activity extends AppCompatActivity {
 
@@ -26,6 +29,15 @@ public class agenda_activity extends AppCompatActivity {
         agenda_events.setLayoutManager(layoutManager);
 
         ArrayList<AgendaEntry> events = new ArrayList<>();
+        try {
+            events = WebAPI.getAllAgendaEntries(CommonStorage.vender_id);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        }
 
         RecyclerView.Adapter adapter = new agenda_adapter(events);
         agenda_events.setAdapter(adapter);
